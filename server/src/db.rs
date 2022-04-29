3a33dd1361a1602c9a4346de5e9fb534a161ca0a
Ondolin <developer@ondolin.de>
2022-04-29 09:21:09 +0200

    added website added authentication removed ssl

--- server/src/db.rs
+++ server/src/db.rs
@@ -16,7 +16,6 @@ pub struct StateLog {
 }
 
 pub fn establish_connection() -> SqliteConnection {
-    dotenv().ok();
 
     let database_url = env::var("DATABASE_URL")
         .expect("DATABASE_URL must be set");
--- server/src/main.rs
+++ server/src/main.rs
@@ -13,21 +13,44 @@ mod db_schema;
 
 use std::sync::Mutex;
 
+use actix_web::dev::ServiceRequest;
+use dotenv::dotenv;
+use std::env;
+
+use actix_web::dev::Service;
+use actix_web::dev::ServiceResponse;
+use actix_web::http::StatusCode;
 use diesel::prelude::*;
+
+use actix_web::dev::Response;
 
 use self::server::MyWebSocket;
 use self::state::{State, CURRENT_STATE, RECIEVER_ADDRS};
 
 use actix_web::{
-    middleware, post, web, App, Error, HttpRequest, HttpResponse, HttpServer, Responder,
+    middleware, post, get, web, App, Error, HttpRequest, HttpResponse, HttpServer, Responder,
 };
 use actix_web_actors::ws;
 use actix_cors::Cors;
 
-use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
+use actix_web::http::Method;
+
+use futures_util::future::{self, Either, FutureExt};
+
+// use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
 
 async fn echo_ws(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
     ws::start(MyWebSocket::new(), &req, stream)
+}
+
+#[get("/current")]
+async fn get_current_status() -> impl Responder {
+
+    let current = CURRENT_STATE.lock().unwrap();
+
+    let state = current.to_str().to_string();
+
+    HttpResponse::Ok().body(state)
 }
 
 #[post("/current")]
@@ -60,32 +83,77 @@ async fn set_state(db_connection: web::Data<Mutex<SqliteConnection>>, req_body: 
     }
 }
 
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    
+    dotenv().ok();
+
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
 
     log::info!("starting HTTP server at http://localhost:8080");
 
-    let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
-    builder
-        .set_private_key_file("cert.key", SslFiletype::PEM)
-        .unwrap();
-    builder.set_certificate_chain_file("cert.crt").unwrap();
+    // let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
+    // builder
+    //     .set_private_key_file("cert.key", SslFiletype::PEM)
+    //     .unwrap();
+    // builder.set_certificate_chain_file("cert.crt").unwrap();
 
     let db_connection = web::Data::new(Mutex::new(db::establish_connection()));
 
     HttpServer::new(move || {
-        let cors = Cors::default()
-              .allow_any_origin();
+
+        let cors = Cors::permissive();
 
         App::new()
-            .service(web::resource("/ws").route(web::get().to(echo_ws)))
+            .service(web::resource("/live").route(web::get().to(echo_ws)))
+            .service(get_current_status)
+            .wrap_fn(|req, srv| {
+
+                let mut access_allowed = true;
+
+                if req.method() == Method::POST {
+
+                    access_allowed = false;
+
+                    let auth_header = req.headers().get(actix_web::http::header::AUTHORIZATION);
+
+                    if let Some(token) = auth_header {
+
+                        let mut token = token.to_str().unwrap().split(" ");
+
+                        token.next();
+
+                        let token = token.next();
+
+                        if let Some(token) = token {
+                            if token == "xw6k2YF4t5yCMky2DYxa7NV" {
+                                access_allowed = true;
+                            }
+                        }
+    
+
+                    }
+
+                }
+
+                if access_allowed {
+                    Either::Left(srv.call(req).map(|res| res))
+                } else {
+                    return Either::Right(future::ready(Ok(req.into_response(
+                        HttpResponse::Forbidden()
+                    ))))
+                }
+
+             })
             .app_data(db_connection.clone())
             .service(set_state)
+            .service(actix_files::Files::new("/", "./website").show_files_listing().index_file("index.html"))
             .wrap(cors)
             .wrap(middleware::Logger::default())
     })
-    .bind_openssl("0.0.0.0:443", builder)?
+    // .bind_openssl(format!("0.0.0.0:{}", env::var("SERVER_PORT").expect("You have to set a server port!")), builder)?
+    .bind(format!("0.0.0.0:{}", env::var("SERVER_PORT").expect("You have to set a server port!")))?
     .run()
     .await
 }
